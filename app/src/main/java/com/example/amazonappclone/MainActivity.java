560e0243704d5dced4c14d5fb912eba51d676bf3
Pawan Singh <pawanharariya@gmail.com>
2019-08-25 15:09:14 +0530

    cart_list_adapter add

--- app/src/main/java/com/example/amazonappclone/MainActivity.java
+++ app/src/main/java/com/example/amazonappclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.amazonappclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -144,7 +145,10 @@ public class MainActivity extends AppCompatActivity
     //Opens the cart activity
     public void openCart(View view){
         int numberOfItems = 3;
-        
+        Intent opencart = new Intent(this,Cart.class);
+        startActivity(opencart);
+
+
 
     }
 }
